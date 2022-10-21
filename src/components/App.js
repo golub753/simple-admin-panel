f53ce8cea9f9352a43d3485cab90a9cbd51e2cc2
Anton Holub <golubanton753@gmail.com>
2022-10-21 21:56:08 +0300

    feat: create basic functional for users list: viewer, edit, delete, create

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,13 @@
+import { Admin, Resource } from "react-admin";
+import { UserList, UserEdit, UserCreate } from "./Users/Users";
+import jsonServerProvider from 'ra-data-json-server';
+
+const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App = () => {
   return ( 
-    <div>
-      test
-    </div>
+    <Admin dataProvider={dataProvider}>
+      <Resource name='users' list={UserList} edit={UserEdit} create={UserCreate} />
+    </Admin>
    );
 }
  
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
+import Normalize from 'react-normalize';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Normalize/>
     <App />
   </React.StrictMode>
 );
